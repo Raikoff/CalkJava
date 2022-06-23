af5d94bc6c33eaf51885d29997ec545555f22310
Raikoff <podchernin.ivan@gmail.com>
2022-06-23 15:00:48 +0300

    update v0.2

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 /*   Создать калькулятор для работы с рациональными и комплексными числами, организовать меню, 
@@ -8,33 +9,14 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        Input a = inputValues();
-
-        Logic.logic(a.n1, a.n2, a.action);
+        Scanner ch = new Scanner(System.in);
+        System.out.print("Введите 1 для рациональных чисел.\nВведите 2 для комплексных чисел.\nВыбор: ");
+        String chois = ch.next();
 
        
-    }
 
-    public static Input inputValues() {  //Вводим значения  и  знак выражения
-
-        Scanner in = new Scanner(System.in);
-        System.out.print("Число X: ");
-        Double n1 = in.nextDouble();
-
-        System.out.print("Число Y: ");
-        Double n2 = in.nextDouble();
-
-        System.out.print("Действие: ");
-        String action = in.next();
-
-        System.out.printf("Вы ввели: %f \n", n1);
-        System.out.printf("Вы ввели: %f \n", n2);
-        System.out.printf("Вы ввели: %s \n", action);
-
-        in.close();
-
-        return new Input(n1, n2, action);
-
+        Input.chois(chois);
+        ch.close();
     }
 
 }
